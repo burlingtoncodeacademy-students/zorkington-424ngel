6e27b839f9b4fb02b162bd47ea6d4b15206e2fb6
424ngel <angel.aguilarestra@gmail.com>
2023-04-30 14:34:11 -0400

    Added the ability for items to store other items

--- index.js
+++ index.js
@@ -18,31 +18,63 @@ class Room {
 }
 
 class Item {
-  constructor(name, description, read, actions) {
+  constructor(name, description, inspect, actions, isStorage, storageType, storage) {
     this.name = name
     this.description = description
-    this.read = read
+    this.inspect = inspect
     this.actions = actions
+    this.isStorage = isStorage
+    this.storageType = storageType
+    this.storage = storage
 }
 }
  // ________________________________________________________________________
 
 
- // OBJECTS
-const note = new Item(
+ // ITEMS
+let note = new Item(
   'note',
   'A NOTE sits on the counter.',
   "\n\nDear valued customer,\nWe wanted to express our sincere gratitude for you offer to help us find our missing family recipe book.\nAs a token of our appreciation, we have left a special surprise for you to discover somewhere in the cafe.\nWe hope it brings a smile to your face :)\nThank you again for you unwavering support. We truly appreciate it.\n\nWarm Regards,\nThe Brown Bear Cafe Owners\n\n",
-  ['read', 'take']
+  ['inspect', 'take'],
+  false,
+  null,
+  []
   )
 
-const pastry = new Item(
+let pastry = new Item(
   'pastry',
   'A PASTRY sits on the table.',
   "\n>> Wow! I can't believe they left me my favorite pastry! I'm so grateful. <<",
-  ['take']
+  ['inspect', 'take'],
+  false,
+  null,
+  []
 )
 
+let pottedPlant = new Item (
+  'plant',
+  "A potted PLANT sits next to the door. There's something suspicious about it...",
+  "",
+  ['inspect'],
+  true,
+  'storage',
+  ['stone']
+)
+
+let stone = new Item (
+  'stone',
+  '\nA STONE with some writing inscribed on it sits inside of the pot. I wonder what it says?',
+  "\nI'm a stone with letters so old,\nMy writing tells of secrets untold.\nFollow the recipe that you seek,\nMy code unlocks the door with a creak.\nThe first three numbers are one, two, four,\nThe last one's odd, but not a bore.\nArrange them well and heed my sign,\nUnlock the door and what you seek you'll find.\n",
+  ['inspect', 'take'],
+  true,
+  'stored',
+  ['plant']
+)
+ // ________________________________________________________________________
+
+
+// ROOMS
 const mainDining = new Room (
   "Main Dining Room",
   "\nYou are in the MAIN DINING area. You are facing the entrance to the PATIO. Behind you is a door to the KITCHEN.",
@@ -52,13 +84,19 @@ const mainDining = new Room (
 const patio = new Room (
   "Patio",
   "\nYou are on the PATIO. Behind you is the MAIN DINING room",
-  []
+  [pottedPlant, stone]
 )
 
 const kitchen = new Room (
   "Kitchen",
   "\nYou are in the KITCHEN. The MAIN DINING room is in front of you.",
   [pastry]
+)
+
+const basement = new Room (
+  "Basement",
+  "\nYou are in the BASEMENT. The KITCHEN is just up the stairs.",
+  []
 )
  // ________________________________________________________________________
 
@@ -67,18 +105,22 @@ const kitchen = new Room (
 const stateMachine = {
   'main dining': ['patio', 'kitchen'],
   'patio': ['main dining'],
-  'kitchen': ['main dining']
+  'kitchen': ['main dining'],
+  'basement': ['kitchen']
 }
 
 const locationLookup = {
   'main dining': mainDining,
   'patio': patio,
-  'kitchen': kitchen
+  'kitchen': kitchen,
+  'basement': basement
 }
 
 const objLookup = {
   'note': note,
-  'pastry': pastry
+  'pastry': pastry,
+  'plant': pottedPlant,
+  'stone': stone
 }
  // ________________________________________________________________________
 
@@ -88,7 +130,7 @@ async function start() {
 
   // INITIALIZE
   let currentRoom = 'main dining'
-  const validCommands = ['help', 'move', 'read', 'take', 'i', 'exit']
+  const validCommands = ['help', 'i', 'move', 'inspect', 'take', 'exit']
   const strCommands = `\n    VALID COMMANDS:    \n` + validCommands.join(', ')
   const askforInput = '\nWhat would you like to do next? '
   let inventory = []
@@ -128,14 +170,25 @@ function doAction(input, location, inv) { // This is a function that does someth
   let entity = input[1]
   if (input.length > 2) entity = input.slice(1).join(' ') // If the room / item is two words or more
     
-  if (action === 'read') {  
-    console.log(objLookup[entity].read)
+  if (action === 'inspect') { 
+    if (objLookup[entity].isStorage === true) {
+      if (objLookup[entity].storage.length === 0) {
+        objLookup[entity].inspect = '\nThis is empty now!'
+      } else {
+        objLookup[entity].storage.forEach (item => console.log(objLookup[item].description))
+      }
+    }
+    console.log(objLookup[entity].inspect)
   } else if (action === 'take') {
     objLookup[entity].description = ''
+
     if (inv.includes(entity)) {
       console.log('\nYou already took this item')
     } else {
-      console.log(objLookup[entity].read)
+      if (objLookup[entity].isStorage === true) {
+        let container = objLookup[entity].storage[0]
+        objLookup[container].storage = objLookup[container].storage.filter (item => item !== entity)
+      }
       inv.push(objLookup[entity].name)
       console.log(`\nYou take the ${entity}. You can now find it in your inventory.`)
     }
@@ -143,7 +196,11 @@ function doAction(input, location, inv) { // This is a function that does someth
     console.log(`\nMoving to ${entity}...`)
     location = entity
     console.log(locationLookup[location].description)
-    locationLookup[location].items.forEach (itm => console.log(itm.description))
+    locationLookup[location].items.forEach (itm => { 
+      if (itm.storageType === 'storage' || itm.storageType === null){
+        console.log(itm.description)
+      }
+    })
   }
 
   return location
@@ -152,17 +209,19 @@ function doAction(input, location, inv) { // This is a function that does someth
 
 
 function validateInput(input, validcmds, location, inv, strcmds) { // Function that validates all user input - returns true or false
+  input = input.toLowerCase()
   let action = input.split(' ')[0]
   let entity = input.split(' ')[1]
 
   if (input.split(' ').length > 2) entity = input.split(' ').slice(1).join(' ') // If room / item is two words
 
   let availItems = locationLookup[location].items.map(obj => obj.name)
-  if (action === 'i') {
+
+  if (action === 'i') { // Displays inventory
     console.log(`\n INVENTORY:`)
     inv.forEach(item => console.log(`    ${item}`))
     return false
-  } else if (action === 'help'){
+  } else if (action === 'help'){ // Displays help info
     console.log(strcmds)
     console.log(`\n>> Enter (command + item/room) to interact with your surroundings. <<\n>> Valid items/rooms are in all uppercase letters. <<`)
     return false
@@ -178,8 +237,12 @@ function validateInput(input, validcmds, location, inv, strcmds) { // Function t
   } else if (action === 'move' && stateMachine[location].includes(entity)) { // Check if player input action is move and the room IS available
     return true
   } else if (!availItems.includes(entity)) { // Check if item exists in that room
-    console.log(`\nI can't find a "${entity}" in this room...`)
+    if (inv.includes(entity) && objLookup[entity].actions.includes(action)) { // Check if item exists in invetory & if action can be used on that item
+      return true
+    } else {
+    console.log(`\nI can't find a "${entity}" in this room or in my inventory...`)
     return false
+    }
   } else if (!objLookup[entity].actions.includes(action)) { // Check if action can be used on that item.
     console.log(`\nI can't ${action} this item.`)
     return false
